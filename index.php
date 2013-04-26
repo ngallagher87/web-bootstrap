c86181492330548a5068bd254e3c8fc732d72c7a
ngallagher87 <nathan@discoverysoftware.com>
2013-04-25 21:37:28 -0700

    Adding initial bootstrap index file

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/font-awesome/css/font-awesome.min.css">
+		<!-- Add any google font familys here -->
+		<link href='http://fonts.googleapis.com/css?family=' rel='stylesheet' type='text/css'>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<!--[if lt IE 9]>
+			<script src="//html5shiv.googlecode.com/svn/trunk/html5.js" onload="window.ieshiv=true;"></script>
+		<![endif]-->
+	</head>
+<!--[if IE 8 ]>    <body class="ie8">    <![endif]-->
+<!--[if IE 9 ]>    <body class="ie9">    <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><body><!--<![endif]-->
+
+<!-- Load js scripts here -->
+<script src="js/jquery.easings.1.3.min.js"></script>
+<script src="js/main.min.js"></script>
+</body>
+</html>
